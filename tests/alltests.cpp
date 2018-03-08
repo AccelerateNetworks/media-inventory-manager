086381065832c66fb416717cc182207d4195d109
Aidan Hahn <aidanjacobhahn@gmail.com>
2018-03-07 22:34:38 -0800

    thats not how you use header files...

--- tests/alltests.cpp
+++ tests/alltests.cpp
@@ -14,18 +14,17 @@ using namespace std;
  * @return
  */
 int main() {
-  
+
   cout << "[+] Running all tests -" << endl;
   cout << "\t\t hashTable testing START" << endl;
   hashTableTest();
   cout << "\t\t hashTable testing END" << endl;
-  
+
 //  cout << "\t\t class Movie testing START" << endl;
 //  // the bool parameter is used to toggle verbose  mode of test functions
 //  movie_testing(false); // try/catch block is inside this function
 //  cout << "\t\t class Movie testing END" << endl;
-  
-  
+
   return 0;
 }
 
