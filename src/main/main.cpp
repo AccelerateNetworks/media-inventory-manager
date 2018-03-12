5c998ee6c6518eb0f3a0d031a9572747986dc8bb
Aidan Hahn <aidanjacobhahn@gmail.com>
2018-03-11 20:40:42 -0700

    fuck my life

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
-
+#include <sstream>
+#include <vector>
 
 #include "../client.h"
 #include "../hashtable.h"
@@ -13,11 +14,17 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::ifstream;
+using std::vector;
 
 Inventory& command_input_file( Inventory& i );
 Inventory& client_input_file( Inventory& i );
 Inventory& movie_input_file( Inventory& i );
 
+int iHasher(int arg){
+  return arg % 5;
+}
+
+HashTable<int, Client> clientLookup(iHasher);
 
 int main () {
 
@@ -39,7 +46,56 @@ int main () {
  */
 Inventory& command_input_file( Inventory& i){
   ifstream inpfile = ifstream("../../docs/data4commands.txt");
-  
+  string line;
+  while(std::getline(inpfile, line)){
+
+    vector<string> tokens;
+    string temp;
+
+    // remove commas
+    for(char c : line){
+      if(c == ' ' || c == ','){
+        if(!temp.empty()){
+          tokens.push_back(temp);
+          temp = "";
+        }
+      }
+
+      temp += std::to_string(c);
+    }
+
+
+    switch(tokens[0].c_str[0]){
+    case 'I': i.printAllMedia();
+      break;
+    case 'H':
+      Client* c = clientLookup.get(tokens[1]);
+      if(c == nullptr){
+        cout << "Client does not exist!" << endl;
+        break;
+      }
+      i.customerHistory(*c);
+      break;
+    case 'B':
+      Client c = clientLookup.get(tokens[1]);
+      if(c == nullptr){
+        cout << "Client does not exist!" << endl;
+        break;
+      }
+      if(tokens[2] != "D"){
+        cout << "Invalid Media COde" << endl;
+        break;
+      }
+      switch()
+
+      break;
+    case default:
+      cout << "invalid input" << endl;
+    }
+
+  }
+
+  return i;
 }
 
 /**  We need to initialize the Inventory object outside of this function
@@ -54,7 +110,30 @@ Inventory& command_input_file( Inventory& i){
  */
 Inventory& client_input_file( Inventory& i){
   ifstream inpfile = ifstream("../../docs/data4customers.txt");
+  string line;
+  while(std::getline(inpfile, line)){
 
+    vector<string> tokens;
+    string temp;
+
+    // remove commas
+    for(char c : line){
+      if(c == ' ' || c == ','){
+        if(!temp.empty()){
+          tokens.push_back(temp);
+          temp = "";
+        }
+      }
+
+      temp += std::to_string(c);
+    }
+
+    for(string tok : tokens){
+      // PROCESS COMMAND
+    }
+  }
+
+  return i;
 }
 
 /**  We need to initialize the Inventory object outside of this function
@@ -69,7 +148,29 @@ Inventory& client_input_file( Inventory& i){
  */
 Inventory& movie_input_file( Inventory& i){
   ifstream inpfile = ifstream("../../docs/data4movies.txt");
+  string line;
+  while(std::getline(inpfile, line)){
 
+    vector<string> tokens;
+    string temp;
+
+    // remove commas
+    for(char c : line){
+      if(c == ' ' || c == ','){
+        if(!temp.empty()){
+          tokens.push_back(temp);
+          temp = "";
+        }
+      }
+
+      temp += std::to_string(c);
+    }
+
+    for(string tok : tokens){
+      // PROCESS COMMAND
+    }
+  }
+
+  return i;
 }
 
-   
